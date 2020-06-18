81b771b81e88dd661b596bbaacc810a53a50b97c
Mr.Hope <zhangbowang1998@gmail.com>
2020-06-18 23:18:35 +0800

    fix: types

--- packages/theme/types/extend.d.ts
+++ packages/theme/types/extend.d.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/no-empty-interface */
 import { HopeThemeConfig } from "./hopeConfig";
-import { HopeI18nConfig } from "@mr-hope/vuepress-shared-utils";
+// import { HopeI18nConfig } from "@mr-hope/vuepress-shared-utils";
 
 declare module "@mr-hope/vuepress-types" {
   interface ThemeConfig extends HopeThemeConfig {
     /* nothing more extends */
   }
 
-  interface I18nConfig extends HopeI18nConfig {
-    /* nothing more extends */
-  }
+  // interface I18nConfig extends HopeI18nConfig {
+  //   /* nothing more extends */
+  // }
 }
 
 declare module "vue/types/vue" {
